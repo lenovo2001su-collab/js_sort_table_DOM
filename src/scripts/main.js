d84cc76983b068b4c6077b62635d27e510095a6a
Udovskiy Sehii <Udovskiy1212@gmail.com>
2026-01-30 17:03:10 +0200

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,6 +4,8 @@ const table = document.querySelector('table');
 const thead = table.querySelector('thead');
 const tbody = table.querySelector('tbody');
 
+let sortDirection = 1;
+
 thead.addEventListener('click', (e) => {
   const th = e.target.closest('th');
 
@@ -14,18 +16,20 @@ thead.addEventListener('click', (e) => {
   const columnIndex = th.cellIndex;
   const rows = Array.from(tbody.rows);
 
+  sortDirection *= -1;
+
   rows.sort((rowA, rowB) => {
     const a = rowA.cells[columnIndex].textContent.trim();
     const b = rowB.cells[columnIndex].textContent.trim();
 
-    const numA = Number(a.replace(/[^0-9.]/g, ''));
-    const numB = Number(b.replace(/[^0-9.]/g, ''));
+    //  const numA = Number(a.replace(/[^0-9.]/g, ''));
+    //  const numB = Number(b.replace(/[^0-9.]/g, ''));
 
-    if (!isNaN(numA) && !isNaN(numB)) {
-      return numA - numB;
-    }
+    //  if (!isNaN(numA) && !isNaN(numB)) {
+    //  return (numA - numB) * sortDirection;
+    //  }
 
-    return a.localeCompare(b);
+    return a.localeCompare(b, 'uk', { numeric: true }) * sortDirection;
   });
 
   tbody.append(...rows);
